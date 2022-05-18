8e78ae48764f99f457bb355dc3f23345de0871f3
U_SAY <sekaino.usay@gmail.com>
2022-05-18 17:48:15 +0900

    ver1.0

--- index.php
+++ index.php
@@ -4,14 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>9 splitter</title>
+    <title>9 Splitter for Instagram</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&family=Pacifico&display=swap" rel="stylesheet">
+    <!-- Google Adsense -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7363201401295069" crossorigin="anonymous"></script>
+    <script data-ad-client="ca-pub-7363201401295069" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body>
-    <form action="./split.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="img" accept=".png,.jpg,.jpeg" required>
-        <button type="submit">split</button>
-    </form>
+    <a href="./index.php"><img src="./img/logo.png" width="10%" alt="9 Splitter for Instagram"></a>
+    <a href="./index.php" style="color: black; text-decoration: none;">
+        <h1>9 Splitter for Instagram</h1>
+    </a>
+    <div>
+        <p>画像を3×3に9等分します．</p>
+        <p>正方形の画像を選択して，「Split!」ボタンを押してください．ファイルの形式は jpg（jpeg）か png にしてください．</p>
+        <form action="./split.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="img" accept=".png,.jpg,.jpeg" required>
+            <br>
+            <button type="submit">Split!</button>
+        </form>
+    </div>
+    <footer>
+        <small>
+            <p>Source code is available on <a href="https://github.com/sekaino-usay/9-splitter" target="_blank">GitHub</a></p>
+            <p>Copyright © 2022 <a href=" https://9split.usay05.com">9 Splitter</a> All Rights Reserved.</p>
+        </small>
+    </footer>
 </body>
 
 </html>
--- split.php
+++ split.php
@@ -1,162 +1,200 @@
-<?php
-//画像が選択されていない場合はエラーを出力
-if (empty($_FILES['img']['tmp_name'])) {
-    echo '画像が選択されていません';
-    exit;
-}
+<!DOCTYPE html>
+<html lang="ja">
 
-//画像を受け取り
-$oimg = $_FILES['img'];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>9 Splitter for Instagram</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru&family=Pacifico&display=swap" rel="stylesheet">
+    <!-- Google Adsense -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7363201401295069" crossorigin="anonymous"></script>
+    <script data-ad-client="ca-pub-7363201401295069" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
 
-//画像のmineタイプを取得
-$mine = mime_content_type($oimg['tmp_name']);
+<body>
+    <a href="./index.php"><img src="./img/logo.png" width="10%" alt="9 Splitter for Instagram"></a>
+    <a href="./index.php" style="color: black; text-decoration: none;">
+        <h1>9 Splitter for Instagram</h1>
+    </a>
+    <div>
 
-if ($mine == 'image/jpeg') { //画像のmineタイプがimage/jpegの場合
-    img_split_jpg();
-} elseif ($mine == 'image/png') { //画像のmineタイプがimage/pngの場合
-    img_split_png();
-} else { //画像のmineタイプがimage/jpeg以外の場合
-    echo "この画像のmineタイプは" . $mine . "です．jpg（jpeg）またはpngのを選択してください．";
-    exit;
-}
-
-//画像処理の関数を作成
-//mineタイプがimage/jpegの場合の関数
-function img_split_jpg()
-{
-    $oimg = $_FILES['img'];
-    $img = imagecreatefromjpeg($oimg['tmp_name']);
-
-    //画像のハッシュ値を取得
-    $hash = hash_file('md5', $oimg['tmp_name']);
-
-    //画像を3x3に分割
-    $img_x = imagesx($img);
-    $img_y = imagesy($img);
-    $img_x_3 = $img_x / 3;
-    $img_y_3 = $img_y / 3;
-
-    //画像を3x3に分割した画像を配列に格納
-    $img_array = array();
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $img_array[$i][$j] = imagecreatefromjpeg($oimg['tmp_name']);
-            imagecopy($img_array[$i][$j], $img, 0, 0, $img_x_3 * $j, $img_y_3 * $i, $img_x_3, $img_y_3);
+        <?php
+        //画像が選択されていない場合はエラーを出力
+        if (empty($_FILES['img']['tmp_name'])) {
+            echo '画像が選択されていません．';
+            exit;
         }
-    }
 
-    //画像を3x3に分割した画像をリサイズ
-    $img_x_3_2 = $img_x;
-    $img_y_3_2 = $img_y;
+        //画像を受け取り
+        $oimg = $_FILES['img'];
 
-    //リサイズ後の画像を配列に格納
-    $img_array_2 = array();
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $img_array_2[$i][$j] = imagecreatetruecolor($img_x_3_2, $img_y_3_2);
-            imagecopyresampled($img_array_2[$i][$j], $img_array[$i][$j], 0, 0, 0, 0, $img_x_3_2, $img_y_3_2, $img_x_3, $img_y_3);
+        //画像のmineタイプを取得
+        $mine = mime_content_type($oimg['tmp_name']);
+
+        if ($mine == 'image/jpeg') { //画像のmineタイプがimage/jpegの場合
+            img_split_jpg();
+        } elseif ($mine == 'image/png') { //画像のmineタイプがimage/pngの場合
+            img_split_png();
+        } else { //画像のmineタイプがimage/jpeg以外の場合
+            echo "この画像のmineタイプは" . $mine . "です．jpg（jpeg）またはpngのを選択してください．";
+            exit;
         }
-    }
 
-    //ディレクトリを作成
-    $dir = "./data/" . $hash . "/";
-    if (!file_exists($dir)) {
-        mkdir($dir);
-    }
+        //画像処理の関数を作成
+        //mineタイプがimage/jpegの場合の関数
+        function img_split_jpg()
+        {
+            $oimg = $_FILES['img'];
+            $img = imagecreatefromjpeg($oimg['tmp_name']);
 
-    //画像を3x3に分割した画像をリサイズした画像を保存
-    $n = 10;
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $n = $n - 1;
-            imagepng($img_array_2[$i][$j], $dir . "split-" . $n . ".jpg");
-        }
-    }
+            //画像のハッシュ値を取得
+            $hash = hash_file('md5', $oimg['tmp_name']);
 
-    //画像を3x3に分割した画像をリサイズした画像をzip圧縮して保存
-    $zip = new ZipArchive();
-    $zip_name = $dir . "9-split.zip";
-    if ($zip->open($zip_name, ZipArchive::CREATE) === TRUE) {
-        $n = 10;
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 3; $j++) {
-                $zip->addFile($dir . "split-" . $n . ".jpg", "split-" . $n . ".jpg");
-                $n = $n - 1;
+            //画像を3x3に分割
+            $img_x = imagesx($img);
+            $img_y = imagesy($img);
+            $img_x_3 = $img_x / 3;
+            $img_y_3 = $img_y / 3;
+
+            //画像を3x3に分割した画像を配列に格納
+            $img_array = array();
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $img_array[$i][$j] = imagecreatefromjpeg($oimg['tmp_name']);
+                    imagecopy($img_array[$i][$j], $img, 0, 0, $img_x_3 * $j, $img_y_3 * $i, $img_x_3, $img_y_3);
+                }
             }
-        }
-        $zip->close();
-    }
 
-    //画像を3x3に分割した画像をリサイズした画像をzip圧縮してダウンロード
-    echo '<a href="' . $zip_name . '">ダウンロード</a>';
-}
+            //画像を3x3に分割した画像をリサイズ
+            $img_x_3_2 = $img_x;
+            $img_y_3_2 = $img_y;
 
-//mineタイプがimage/pngの場合の関数
-function img_split_png()
-{
-    $oimg = $_FILES['img'];
-    $img = imagecreatefrompng($oimg['tmp_name']);
-
-    //画像のハッシュ値を取得
-    $hash = hash_file('md5', $oimg['tmp_name']);
-
-    //画像を3x3に分割
-    $img_x = imagesx($img);
-    $img_y = imagesy($img);
-    $img_x_3 = $img_x / 3;
-    $img_y_3 = $img_y / 3;
-
-    //画像を3x3に分割した画像を配列に格納
-    $img_array = array();
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $img_array[$i][$j] = imagecreatefrompng($oimg['tmp_name']);
-            imagecopy($img_array[$i][$j], $img, 0, 0, $img_x_3 * $j, $img_y_3 * $i, $img_x_3, $img_y_3);
-        }
-    }
-
-    //画像を3x3に分割した画像をリサイズ
-    $img_x_3_2 = $img_x;
-    $img_y_3_2 = $img_y;
-
-    //リサイズ後の画像を配列に格納
-    $img_array_2 = array();
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $img_array_2[$i][$j] = imagecreatetruecolor($img_x_3_2, $img_y_3_2);
-            imagecopyresampled($img_array_2[$i][$j], $img_array[$i][$j], 0, 0, 0, 0, $img_x_3_2, $img_y_3_2, $img_x_3, $img_y_3);
-        }
-    }
-
-    //ディレクトリを作成
-    $dir = "./data/" . $hash . "/";
-    if (!file_exists($dir)) {
-        mkdir($dir);
-    }
-
-    //画像を3x3に分割した画像をリサイズした画像を保存
-    $n = 10;
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
-            $n = $n - 1;
-            imagepng($img_array_2[$i][$j], $dir . "split-" . $n . ".jpg");
-        }
-    }
-
-    //画像を3x3に分割した画像をリサイズした画像をzip圧縮して保存
-    $zip = new ZipArchive();
-    $zip_name = $dir . "9-split.zip";
-    if ($zip->open($zip_name, ZipArchive::CREATE) === TRUE) {
-        $n = 10;
-        for ($i = 0; $i < 3; $i++) {
-            for ($j = 0; $j < 3; $j++) {
-                $zip->addFile($dir . "split-" . $n . ".jpg", "split-" . $n . ".jpg");
-                $n = $n - 1;
+            //リサイズ後の画像を配列に格納
+            $img_array_2 = array();
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $img_array_2[$i][$j] = imagecreatetruecolor($img_x_3_2, $img_y_3_2);
+                    imagecopyresampled($img_array_2[$i][$j], $img_array[$i][$j], 0, 0, 0, 0, $img_x_3_2, $img_y_3_2, $img_x_3, $img_y_3);
+                }
             }
-        }
-        $zip->close();
-    }
 
-    //画像を3x3に分割した画像をリサイズした画像をzip圧縮してダウンロード
-    echo "<a href='" . $zip_name . "' download='" . $zip_name . "'>ダウンロード</a>";
-}
+            //ディレクトリを作成
+            $dir = "./data/" . $hash . "/";
+            if (!file_exists($dir)) {
+                mkdir($dir);
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像を保存
+            $n = 10;
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $n = $n - 1;
+                    imagepng($img_array_2[$i][$j], $dir . "split-" . $n . ".jpg");
+                }
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像をzip圧縮して保存
+            $zip = new ZipArchive();
+            $zip_name = $dir . "9-split.zip";
+            if ($zip->open($zip_name, ZipArchive::CREATE) === TRUE) {
+                $n = 9;
+                for ($i = 0; $i < 3; $i++) {
+                    for ($j = 0; $j < 3; $j++) {
+                        $zip->addFile($dir . "split-" . $n . ".jpg", "split-" . $n . ".jpg");
+                        $n = $n - 1;
+                    }
+                }
+                $zip->close();
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像をzip圧縮してダウンロード
+            echo "<a href='" . $zip_name . "' download='9-split.zip'>ダウンロード</a>";
+        }
+
+        //mineタイプがimage/pngの場合の関数
+        function img_split_png()
+        {
+            $oimg = $_FILES['img'];
+            $img = imagecreatefrompng($oimg['tmp_name']);
+
+            //画像のハッシュ値を取得
+            $hash = hash_file('md5', $oimg['tmp_name']);
+
+            //画像を3x3に分割
+            $img_x = imagesx($img);
+            $img_y = imagesy($img);
+            $img_x_3 = $img_x / 3;
+            $img_y_3 = $img_y / 3;
+
+            //画像を3x3に分割した画像を配列に格納
+            $img_array = array();
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $img_array[$i][$j] = imagecreatefrompng($oimg['tmp_name']);
+                    imagecopy($img_array[$i][$j], $img, 0, 0, $img_x_3 * $j, $img_y_3 * $i, $img_x_3, $img_y_3);
+                }
+            }
+
+            //画像を3x3に分割した画像をリサイズ
+            $img_x_3_2 = $img_x;
+            $img_y_3_2 = $img_y;
+
+            //リサイズ後の画像を配列に格納
+            $img_array_2 = array();
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $img_array_2[$i][$j] = imagecreatetruecolor($img_x_3_2, $img_y_3_2);
+                    imagecopyresampled($img_array_2[$i][$j], $img_array[$i][$j], 0, 0, 0, 0, $img_x_3_2, $img_y_3_2, $img_x_3, $img_y_3);
+                }
+            }
+
+            //ディレクトリを作成
+            $dir = "./data/" . $hash . "/";
+            if (!file_exists($dir)) {
+                mkdir($dir);
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像を保存
+            $n = 10;
+            for ($i = 0; $i < 3; $i++) {
+                for ($j = 0; $j < 3; $j++) {
+                    $n = $n - 1;
+                    imagepng($img_array_2[$i][$j], $dir . "split-" . $n . ".jpg");
+                }
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像をzip圧縮して保存
+            $zip = new ZipArchive();
+            $zip_name = $dir . "9-split.zip";
+            if ($zip->open($zip_name, ZipArchive::CREATE) === TRUE) {
+                $n = 9;
+                for ($i = 0; $i < 3; $i++) {
+                    for ($j = 0; $j < 3; $j++) {
+                        $zip->addFile($dir . "split-" . $n . ".jpg", "split-" . $n . ".jpg");
+                        $n = $n - 1;
+                    }
+                }
+                $zip->close();
+            }
+
+            //画像を3x3に分割した画像をリサイズした画像をzip圧縮してダウンロード
+            echo "<p>分割が正常に完了しました！<br>以下のリンクからZIPファイルをダウンロードし，解凍してご使用ください．</p>";
+            echo "<a href='" . $zip_name . "' download='9-split.zip'>ダウンロード</a>";
+        }
+        ?>
+        <p>何か問題があれば，<a href="https://twitter.com/sekaino_usay" target="_blank">U_SAY</a>までご連絡をお願いします．</p>
+    </div>
+    <footer>
+        <small>
+            <p>Source code is available on <a href="https://github.com/sekaino-usay/Icon-Ukrainer" target="_blank">GitHub</a></p>
+            <p>Copyright © 2022 <a href=" https://icon-ukrainer.usay05.com" target="_blank">Icon Ukrainer</a> All Rights Reserved.</p>
+        </small>
+    </footer>
+</body>
+
+</html>
